d4265d5b8f75680f9324df44fd3e4d8d70556bdb
THEOCKLES <theockles@THEOCKLES.theockles.com>
2021-02-09 08:52:05 -0600

    V-1.0

--- openacademy/__ manifest__.py
+++ openacademy/__ manifest__.py
@@ -1,36 +1,35 @@
-# - * - coding: utf-8 - * - 
-{ 
-'nombre' : "Academia abierta" , 'resumen' : "" "Administrar capacitaciones" "" , 'descripción' : "" "         Módulo de Academia abierta para administrar capacitaciones:             - cursos de formación             - sesiones de formación             - registro de asistentes     "" " , 'autor' : " Mi empresa " , 'sitio web' : " http://www.yourcompany.com " , 
-# Las categorías se pueden utilizar para filtrar módulos en la lista de módulos # Comprobar https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml     
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
 
-     
+    'summary': """Manage trainings""",
 
-     
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
 
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
 
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
 
-
-
-     
-     
-
-    
-    
-    # para la lista completa 
-'categoría' : 'Prueba' , 'versión' : '0.1' , 
-# cualquier módulo necesario para que este funcione correctamente 'depende' : [ 'base' ], # siempre cargó 'datos' : [ # 'security / ir.model.access.csv', 'templates.xml' , ], # solo se cargó en modo de demostración 'demo' : [ 'demo.xml' , ], }     
-     
-
-    
-     
-
-    
-     
-        
-        
-    
-    
-     
-        
-    
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
