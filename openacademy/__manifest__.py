e8b3257bc9fc090668af81f57ac01e6415278bd3
Moises <moiavil56@gmail.com>
2021-02-12 17:24:35 -0600

    Version 4.0

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -27,6 +27,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/openacademy.xml',
+        'views/course.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
