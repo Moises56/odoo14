2fd4f91b6e6eb6fa629a4441856700fdcf9d4e49
Moises <moiavil56@gmail.com>
2021-02-14 08:08:00 -0600

    Version 8.0

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -6,9 +6,10 @@ class Session(models.Model):
     _description = "OpenAcademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
     
     instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
