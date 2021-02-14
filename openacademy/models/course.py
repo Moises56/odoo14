b4863044e8a3abb4d9c67c92f3401b42e711f88f
Moises <moiavil56@gmail.com>
2021-02-14 09:29:04 -0600

    Version 9.2

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -11,3 +11,13 @@ class Course(models.Model):
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
